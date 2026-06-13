3f679c445c0b3f2927145468b608494a30c03497
sanchitaaa10 <2024.sanchitas@isu.ac.in>
2026-06-13 19:19:50 +0530

    edit crop feature

--- app.py
+++ app.py
@@ -20,7 +20,19 @@ def login():
 
 @app.route("/dashboard")
 def dashboard():
-    return render_template("dashboard.html")
+
+    conn = sqlite3.connect("harvestiq.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT COUNT(*) FROM crops")
+    total_crops = cursor.fetchone()[0]
+
+    conn.close()
+
+    return render_template(
+        "dashboard.html",
+        total_crops=total_crops
+    )
 
 @app.route("/add-crop", methods=["GET", "POST"])
 def add_crop():
@@ -46,6 +58,97 @@ def add_crop():
         return "Crop Saved Successfully!"
 
     return render_template("add_crop.html")
+
+@app.route("/test")
+def test():
+
+    conn = sqlite3.connect("harvestiq.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM crops")
+
+    data = cursor.fetchall()
+
+    conn.close()
+
+    return str(data)
+
+
+@app.route("/crops")
+def view_crops():
+
+    conn = sqlite3.connect("harvestiq.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM crops")
+    crops = cursor.fetchall()
+
+    conn.close()
+
+    return render_template(
+        "crops.html",
+        crops=crops
+    )
+@app.route("/delete/<int:id>")
+def delete_crop(id):
+
+    conn = sqlite3.connect("harvestiq.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "DELETE FROM crops WHERE id=?",
+        (id,)
+    )
+
+    conn.commit()
+    conn.close()
+
+    return redirect("/crops")
+
+@app.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit_crop(id):
+
+    conn = sqlite3.connect("harvestiq.db")
+    cursor = conn.cursor()
+
+    if request.method == "POST":
+
+        crop_name = request.form["crop_name"]
+        location = request.form["location"]
+        yield_amount = request.form["yield_amount"]
+
+        cursor.execute("""
+        UPDATE crops
+        SET crop_name=?,
+            location=?,
+            yield_amount=?
+        WHERE id=?
+        """, (
+            crop_name,
+            location,
+            yield_amount,
+            id
+        ))
+
+        conn.commit()
+        conn.close()
+
+        return redirect("/crops")
+
+    cursor.execute(
+        "SELECT * FROM crops WHERE id=?",
+        (id,)
+    )
+
+    crop = cursor.fetchone()
+
+    conn.close()
+
+    return render_template(
+        "edit_crop.html",
+        crop=crop
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
