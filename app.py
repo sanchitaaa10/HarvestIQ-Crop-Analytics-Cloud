27d3cfb110a9471243e2e84e972c3f493660347d
sanchitaaa10 <2024.sanchitas@isu.ac.in>
2026-06-13 17:45:19 +0530

    initial harvestiq setup

--- app.py
+++ app.py
@@ -10,5 +10,10 @@ def login():
 def dashboard():
     return render_template("dashboard.html")
 
+@app.route("/add-crop")
+def add_crop():
+    return render_template("add_crop.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
+
